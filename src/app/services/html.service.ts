9b31594eb0fec2a5b0d4abb6e89647bf4a4c8b5e
Naiihu <guillou.florian@floriangll.fr>
2025-09-02 19:30:48 +0200

    inject instead of constructor call

--- src/app/services/html.service.ts
+++ src/app/services/html.service.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HtmlService {
-  constructor(
-    private httpClient: HttpClient
-  ) {}
+  private httpClient = inject(HttpClient);
 
   public get(path: string): Observable<string>{
     return this.httpClient.get(`/assets/data/templates/${path}.html`, {responseType: 'text'});
