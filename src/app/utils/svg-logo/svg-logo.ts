1e8de2f697724442f915c3e748078da155e732f9
Naiihu <guillou.florian@floriangll.fr>
2025-11-25 23:45:37 +0100

    Changement logo

--- src/app/utils/svg-logo/svg-logo.ts
+++ src/app/utils/svg-logo/svg-logo.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-svg-logo',
   imports: [],
-  templateUrl: './logo.svg',
+  templateUrl: './logo_full.svg',
   styleUrl: './svg-logo.scss'
 })
 export class SvgLogo {
